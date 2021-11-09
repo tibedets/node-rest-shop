a36d110df0c2020b29de6f7dbdff24b7d97c5eb5
Thaddeus Brown <tibedets@gmail.com>
2021-11-08 19:33:26 -0600

    node-rest-server first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+
+//Routes
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+module.exports = app;
